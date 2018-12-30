49e2128565c0d919278a8779d4e35bba1c04680b
Nicola Bortignon <info@nicolabortignon.com>
2018-12-30 22:38:30 +0000

    adding routeManager
    Handles route registration

---
+++ src/utils/routeManager.js
@@ -0,0 +1,24 @@
+class RouteManager {
+  constructor(){
+    this.routes = [];
+  }
+
+ addRoute(server, method, route, controller){
+		server[method](route,controller);
+		this.routes.push({
+			method:method,
+			route:route,
+			controller:controller
+		});
+
+	}
+
+ listRoutes(){
+		for(const route of this.routes){
+			console.log('Method:', route.method, 'Route:', route.route, 'Controller:', route.controller, )
+		}
+	}  
+
+}
+
+module.exports = RouteManager;
