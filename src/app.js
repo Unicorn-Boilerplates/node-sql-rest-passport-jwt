df2c1c156981032f6f1521baff2935288da7cf38
Nicola Bortignon <info@nicolabortignon.com>
2018-12-30 22:38:30 +0000

    adding routeManager
    Handles route registration

--- src/app.js
+++ src/app.js
@@ -1,1 +1,23 @@
-console.log('app loaded')
+const express = require('express');
+const https = require('https');
+const http = require('http');
+const app = express();
+const RouteManager = require('./utils/routeManager')
+const routeManager = new RouteManager();
+
+http.createServer(app).listen(80);
+https.createServer(app).listen(443);
+
+routeManager.addRoute(app,'get','/test', function (req, res) {
+  res.send('test')
+})
+
+
+routeManager.addRoute(app,'get','/test2', function (req, res) {
+  res.send('test2')
+})
+
+routeManager.listRoutes()
+
+
+module.exports = app;
