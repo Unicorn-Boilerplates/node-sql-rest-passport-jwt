95b3076ba7cbca9d7c15cafd8b35ec7ed0d4e5bc
Nicola Bortignon <info@nicolabortignon.com>
2019-01-01 10:26:50 +0000

    Adding ESLinting

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,30 +1,30 @@
 const userModel = require('./../models/user');
 
-const get_user = function(req, res) {
-	userModel.getUser(req.params.id).then(user => {
-		res.setHeader('Content-Type', 'application/json');
+const getUser = function (req, res) {
+  userModel.getUser(req.params.id).then((user) => {
+    res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify(user));
-	})	 
+  });
 };
 
 // Display detail page for a specific Author.
-const get_users = function(req, res) {
-	userModel.getAllUsers().then(users => {
-		res.setHeader('Content-Type', 'application/json');
+const getUsers = function (req, res) {
+  userModel.getAllUsers().then((users) => {
+    res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify(users));
-	})	 
+  });
 };
 
-const get_user_projects = function(req, res) {
-	userModel.getUserProject(req.params.id).then(projects => {
-		res.setHeader('Content-Type', 'application/json');
+const getUserProjects = function (req, res) {
+  userModel.getUserProject(req.params.id).then((projects) => {
+    res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify(projects));
-	})	 
+  });
 };
 
 
 module.exports = {
-	get_user,
-	get_users,
-	get_user_projects
-}
+  getUser,
+  getUsers,
+  getUserProjects,
+};
--- src/models/index.js
+++ src/models/index.js
@@ -1,32 +1,34 @@
 const {
-	DatabaseManager, 
-	BELONGS_TO,
-	HAS_MANY,
-	BELONGS_TO_MANY
-}  = require('./../utils/databaseManager')
-const configurationDatabase = require('../../config/database')
-const models = []
-const databaseManager = new DatabaseManager(configurationDatabase.development); 
+  DatabaseManager,
+  BELONGS_TO,
+  HAS_MANY,
+  BELONGS_TO_MANY,
+} = require('./../utils/databaseManager');
+const configurationDatabase = require('../../config/database');
 
-require('fs').readdirSync(__dirname + '/').forEach(function(file) {
+const models = [];
+const databaseManager = new DatabaseManager(configurationDatabase.development);
+
+
+require('fs').readdirSync(`${__dirname}/`).forEach((file) => {
   if (file.match(/\.js$/) !== null && file !== 'index.js') {
-    var name = file.replace('.js', '');
-    exports[name] = models[name]  = require('./' + file);
+    const name = file.replace('.js', '');
+    exports[name] = models[name] = require(`./${file}`);
   }
 });
 
 
 // Setup all the models
-for(var modelKey in models){
-	const model = models[modelKey]; 
-	databaseManager.registerModel(model)
+for (const modelKey in models) {
+  const model = models[modelKey];
+  databaseManager.registerModel(models[modelKey]);
 }
 
-//ar userdb = databaseManager.registerModel(user.modelName, user.shape)
-//var projectdb = databaseManager.registerModel(project.modelName, project.shape)
-databaseManager.registerRelationship(models['project'],models['user'],BELONGS_TO_MANY, {through: 'UserProject'}, false);
-databaseManager.registerRelationship(models['user'],models['project'],BELONGS_TO_MANY, {through: 'UserProject'}, false);
+// ar userdb = databaseManager.registerModel(user.modelName, user.shape)
+// var projectdb = databaseManager.registerModel(project.modelName, project.shape)
+databaseManager.registerRelationship(models.project, models.user, BELONGS_TO_MANY, { through: 'UserProject' }, false);
+databaseManager.registerRelationship(models.user, models.project, BELONGS_TO_MANY, { through: 'UserProject' }, false);
 
 
-databaseManager.listModels()
+databaseManager.listModels();
 databaseManager.listRelationships();
